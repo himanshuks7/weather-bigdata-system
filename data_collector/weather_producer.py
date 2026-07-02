a12eeb4285c8b6e631eb24c7827492381b83ec93
Himanshu Kumar Sinha <himanshukumarsinha2003@gmail.com>
2026-07-02 14:00:32 +0530

    Configure Kafka producer settings

--- data_collector/weather_producer.py
+++ data_collector/weather_producer.py
@@ -7,4 +7,21 @@ from datetime import datetime, timezone
 
 API_KEY = "YOUR_OPENWEATHER_API_KEY"
 KAFKA_TOPIC = "weather_stream_v3"
-KAFKA_BROKER = "localhost:9092"
+KAFKA_BROKER = "localhost:9092"     
+
+CITIES = [
+    "Delhi",
+    "Mumbai",
+    "Chennai",
+    "Bangalore",
+    "Kolkata",
+    "Hyderabad",
+    "London",
+    "New York",
+    "Tokyo"
+]
+
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BROKER,
+    value_serializer=lambda x: json.dumps(x).encode("utf-8")
+)
